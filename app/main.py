c4abc5ea61298702e2176bef586b246caff6d19e
Neyrony <sserebriy@gmail.com>
2026-03-31 13:32:30 +0300

    Added posixpath for join the path if loop

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import os
+import posixpath
 
 
 def move_file(command: str) -> None:
@@ -10,16 +11,17 @@ def move_file(command: str) -> None:
                                        command_list[1],
                                        command_list[2].split("/"))
 
-    if destination_list[-1].find(".txt") == -1:
+    if destination_list[-1].find(".") == -1:
         destination_list.append(moved_file)
 
     path = ""
 
     for destination in range(len(destination_list) - 1):
-        path += destination_list[destination] + "/"
-        # path = os.path.join(path, destination_list[destination])
+        # path += destination_list[destination] + "/"
+        path = posixpath.join(path, destination_list[destination])
         os.makedirs(path, exist_ok=True)
-
+    if path:
+        path += "/"
     with (
         open(path + destination_list[-1], "w") as file_rename,
         open(moved_file) as file_reader
