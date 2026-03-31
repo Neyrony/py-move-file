8027eb3b00c0ac10f9e6a0ab01dc85635e7fcbd1
Neyrony <sserebriy@gmail.com>
2026-03-31 12:33:07 +0300

    Implemented solution for py-move-file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,24 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    command_list = command.split(" ")
+    if len(command_list) != 3 or command_list[0] != "mv":
+        return
+
+    _, moved_file, destination_list = (command_list[0],
+                                       command_list[1],
+                                       command_list[2].split("/"))
+    path = ""
+
+    for destination in range(len(destination_list) - 1):
+        path += destination_list[destination] + "/"
+        os.makedirs(path, exist_ok=True)
+
+    with (
+        open(path + destination_list[-1], "w") as file_rename,
+        open(moved_file) as file_reader
+    ):
+        file_rename.write(file_reader.read())
+
+    os.remove(moved_file)
